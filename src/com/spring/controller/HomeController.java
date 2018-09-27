72d20b3ad4e685e603f282a8f60e27f3e9fe8d41
Boss <bossthiti.gameillicit01@hotmail.com>
2018-09-27 11:53:07 +0700

    commit

--- src/com/spring/controller/HomeController.java
+++ src/com/spring/controller/HomeController.java
@@ -1,5 +1,28 @@
 package com.spring.controller;
 
-public class HomeController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(HttpSession session, HttpServletRequest request, HttpServletResponse response) {
+		response.setContentType("text/html;charset=UTF-8");
+
+		System.out.println("Home Page Requested");
+		ModelAndView model = new ModelAndView("home");
+		try {
+
+			return model;
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return model;
+	}
 }
